67545aae4ee99464be21fe28b7b02777951bbbfa
mathumuthu <168325420+mathumuthu@users.noreply.github.com>
2025-12-02 10:36:04 +0530

    Add files via upload

---
+++ cloud.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+
+
+
+
+
+const hamburger = document.querySelector(".hamburger");
+const navBar    = document.querySelector(".navBar");
+
+hamburger.addEventListener("click", () => {
+  navBar.classList.toggle("active");
+});
+
